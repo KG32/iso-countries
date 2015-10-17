31083f84890688053547d5d17b46104555fd68db
KG32 <kkgroblewski@gmail.com>
2015-10-17 17:26:55 +0200

    ..

--- package.js
+++ package.js
@@ -1,20 +1,14 @@
 Package.describe({
   name: 'kg32:iso-countries',
   version: '1.0.0',
-  // Brief, one-line summary of the package.
   summary: 'Convert countries to ISO codes and vice versa.',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/KG32/iso-countries.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    //api.versionsFrom('0.9.0');
-    api.use(["templating", "meteor-base"], "client");
-    //api.addFiles("iso-countries.js", "client");
     api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+    api.use(["templating", "meteor-base"], "client");
     api.addFiles(['iso-countries.js']);
     api.export('convertCountry');
     api.export('convertCountryRev');

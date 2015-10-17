05a82bd00038d3e5509c41ac7e14f8c77f8f82c0
KG32 <kkgroblewski@gmail.com>
2015-10-17 16:56:15 +0200

    git

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Convert countries to ISO codes and vice versa.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/KG32/iso-countries.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
